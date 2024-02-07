1dae8c542782867c2de58ed50cfd43330cdebc48
Aasavari12356 <aasavaribedade@gmail.com>
2024-02-07 16:17:54 +0530

    added

---
+++ script2.js
@@ -0,0 +1,57 @@
+//pgm1
+a=90
+console.log(a)
+console.log(typeof a)
+a=90.5
+console.log(a)
+console.log(typeof a)
+
+//pgm2
+k= true
+console.log(k)
+console.log(typeof k)
+
+//pgm3
+first_name="Aasavari"
+console.log(first_name)
+console.log(typeof first_name)
+
+//pgm4
+// <, >, <= , >= 
+// ==, != //value
+// ===, !== // value n type
+
+console.log(2 === "2")
+console.log("2" === 2)
+console.log(2 !== 2)
+console.log(7 > 3)
+console.log(7 < 3)
+console.log(7 >= 7)
+console.log(3 <= 3)
+console.log(1 == 1)
+console.log(2 != 3)
+
+
+//pgm4
+//logical operator
+//&&
+//t n t= t
+//t n f= f
+//f n t= f
+//f n f= f
+
+console.log(2 == 2 && 3 == 3)
+console.log(2 == 2 && 3 == 4)
+console.log(3 == 2 && 4 == 4)
+console.log(1 == 2 && 2 == 3)
+
+// ||
+// t n t= t
+// t n f= t
+// f n t= t
+// f n f= f
+
+console.log(2 == 2 || 3 == 3)
+console.log(2 == 2 || 2 == 3)
+console.log(3 == 2 || 4 == 4)
+console.log(2 == 3 || 1 == 4)
