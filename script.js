851727b88df59e1b5311ca3559f0ab198c654f23
Aasavari12356 <aasavaribedade@gmail.com>
2024-02-07 15:31:55 +0530

    added

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+//pgm1
 console.log("hello")
+console.log("hi")
+
+a= 10
+console.log(a)
+a= 100
+console.log(a)
+
+//pgm2
+const h=1000
+console.log(h)
+
+
+//pgm3
+a1=10
+b1=2
+
+console.log(a1+b1)
+console.log(a1-b1)
+console.log(a1*b1)
+console.log(a1/b1)
+console.log(a1%b1)
+
+function Calculator(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+}
+Calculator(9,3)
+Calculator(100,10)
