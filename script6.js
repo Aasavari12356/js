92ffcd72a94820d72f1130703e4563869a82e282
Aasavari12356 <aasavaribedade@gmail.com>
2024-02-14 21:54:55 +0530

    for

---
+++ script6.js
@@ -0,0 +1,61 @@
+// // for (initialization ; condition ; incremeny/decrement)
+
+// // pgm1
+// for (i=1 ; i<=5 ; i++){
+//     console.log(i)
+// }
+
+// // pgm2
+// for (i=5 ; i>=1 ; i--){
+//     console.log(i)
+// }
+
+// // pgm3
+// for (i=2 ; i<=20 ;i=i+2){
+//     console.log(i)
+// }
+
+// // pgm4
+// for (i=5;i<=50;i=i+5){
+//     console.log(i)
+// }
+
+// // pgm5
+// for (i=20;i>=2;i=i-2){
+//     console.log(i)
+// }
+
+// pgm6
+// pgm with break statement
+for (i=1;i<=5;i++){
+    if(i==3)
+   {
+     break
+   }
+        console.log(i)
+    }
+
+
+// pgm7
+// for (i=5;i>=1;i--){
+//     if(i==2)
+//     {
+//         console.log(i)
+//     }
+// }
+for(let i = 5 ; i >= 1 ; i--){ // 4 // 3
+    if(i == 3){
+        break
+    }
+    console.log(i) // 5 // 4
+}
+
+// pgm8
+for(i=1;i<=5;i++){
+    console.log(i)
+    if(i==3){
+        break
+    }
+           
+    
+}
