140b0ecf74e84b4bf1193d0d3502fed355a2d026
Aasavari12356 <aasavaribedade@gmail.com>
2024-02-16 15:26:12 +0530

    while loop

---
+++ script7.js
@@ -0,0 +1,90 @@
+// // table of 5
+// for(i=5;i<=50;i=i+5){
+//     console.log(i)
+// }
+// // table of reverse 3
+// for (i=30;i>=3;i=i-3){
+//     console.log(i)
+// }
+// // break
+// for(i=1;i<=5;i++){
+//     console.log(i)
+//         if(i==3){
+//             break
+//         }
+// }
+// // 
+// for(i=1;i<=5;i++){
+//     if(i==3){
+//         break
+//     }
+//     console.log(i)
+// }
+
+//while
+// initialization
+// while(cond){
+    // statement
+    // increment n decrement
+// }
+// pgm1
+// (i=1)
+// while(i<=5){
+//     console.log(i)
+//     i=i+1
+// }
+// // pgm2
+// (i=50)
+// while(i>=5){
+//     console.log(i)
+//     i=i-5
+// }
+// // pgm3
+// (i=1)
+// while(i<=5){
+//     if(i==3){
+//         break
+//     }
+//     console.log(i)
+//     i=i+1
+// }
+// // pgm4
+// (i=1)
+// while(i<=5){
+//     console.log(i)
+//     i=i+1
+//     if(i==3){
+//         break
+//     }
+// }
+
+
+// i=1
+// while(i<=5){
+//     console.log(i)
+//     if(i==2){
+//         break
+//     }
+//     i=i+1
+// }
+
+// i=1
+// while(i<=5){
+//     if(i==2){
+//         break
+//     }
+//     console.log(i)
+//     i=i+1
+// }
+
+i=1
+while(i<=5){
+    if(i==3){
+        i++
+        continue
+        
+    }
+    console.log(i)
+    i=i+1
+}
+
