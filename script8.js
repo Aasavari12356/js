8aa9116a62a8b9f2f9d586705e809ba6a44c5248
Aasavari12356 <aasavaribedade@gmail.com>
2024-02-19 13:50:18 +0530

    list aaray

--- script8.js
+++ script8.js
@@ -1,33 +1,87 @@
 // Define array
-let names=['sachin','tejas','kunal','arjun','ishwar']
-console.log(names[0])
-console.log(names[3])
+// let names=['sachin','tejas','kunal','arjun','ishwar']
+// console.log(names[0])
+// console.log(names[3])
 
-// array stores by the index
-console.log(names[2])
+// // array stores by the index
+// console.log(names[2])
 
-// how to find length of array
-q=names.length
-console.log(q)
+// // how to find length of array
+// q=names.length
+// console.log(q)
 
-// pgm2
-animals=['tiger','lion','cat','dog']
-console.log(animals.length)
-console.log(animals[2])
+// // pgm2
+// animals=['tiger','lion','cat','dog']
+// console.log(animals.length)
+// console.log(animals[2])
 
-for(i=0 ; i<=animals.length;i++){
+// for(i=0 ; i<=animals.length;i++){
+//     // console.log(i)
+//     console.log(animals[i])
+// }
+
+// for (i=animals.length;i>=0;i--){
+//     // console.log(i)
+//     console.log(animals[i])
+// }
+
+// // pgm3
+let fruits = ['apple','chikko','banana','cherry']
+// console.log(fruits[0])
+for (let i=0 ; i< fruits.length ; i++){
     // console.log(i)
-    console.log(animals[i])
+    console.log(fruits[i])
 }
 
-for (i=animals.length;i>=0;i--){
-    // console.log(i)
-    console.log(animals[i])
+// in reverse
+for (let i=fruits.length;i>=0;i--){
+    console.log(fruits[i])
 }
 
-// pgm3
-fruits=['chikko','apple','banana','cherry']
-console.log(fruits[0])
-for (i=0;i<=fruits.length;i++){
-    console.log(i)
+// pgm4
+// animals= ['tiger','monkey','lion','elephant']
+// for(let i=0;i< animals.length;i++){
+//     if(animals[i]='lion'){
+//         break
+//     }
+//     console.log(animals[i])
+// }
+
+// let animals = ["tiger","lion","cat","monkey","rabbit"]
+// for(let i = 0 ; i < animals.length ; i++){
+//     //console.log(i)
+//     //console.log(animals[i])
+//     if(animals[i] == "monkey"){
+//         break
+//     }
+//     console.log(animals[i])
+// }
+
+// let floweres = ['lilly','jasmine','rose','tulip']
+// for (let i=0;i< floweres.length;i++){
+//     if(floweres[i]== 'rose'){
+//         continue
+//     }
+//     console.log(floweres[i])
+
+
+// }
+
+country = ["india","pakistan","bangladesh","nepal"]
+console.log(country)
+i=0
+while(i< country.length){
+    console.log(country[i])
+    i=i+1
 }
+
+
+// let country =  ["india","srilanka","bangladesh","nepal"]
+// console.log(country)
+// let y = 0
+// while(y < country.length){
+//     //console.log(y)
+//     console.log(country[y])
+//     y = y + 1
+
+// }
