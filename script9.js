4415543b5ac04cefefbad02fe4c3a3d12b8eace9
Aasavari12356 <aasavaribedade@gmail.com>
2024-02-19 14:10:20 +0530

    array function

---
+++ script9.js
@@ -0,0 +1,32 @@
+// push()
+let names = ["aasavari","shravani","janvi"]
+q1= names.push("shreya")
+console.log(q1)
+console.log(names)
+// unshift()
+q2= names.unshift("monika")
+console.log(names)
+// pop()
+q3= names.pop()
+console.log(names)
+// shift()
+q4= names.shift()
+console.log(names)
+// indexof
+let animals=["tiger","lion","elephant","cat"]
+q5= animals.indexOf("lion")
+console.log(q5)
+//include()
+ q6= animals.includes("cat")
+console.log(q6)
+// reverse()
+q7= animals.reverse()
+console.log(q7)
+// at()
+q8= animals.at(2)
+console.log(animals)
+// concat
+a=[11,22,33]
+b=[44,55,66]
+q9= a.concat(b)
+console.log(q9)
