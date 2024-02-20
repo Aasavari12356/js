3507bbc7b25bde7b1f67f005663f3ea312758567
Aasavari12356 <aasavaribedade@gmail.com>
2024-02-20 12:21:37 +0530

    array method

--- script9.js
+++ script9.js
@@ -30,3 +30,51 @@ a=[11,22,33]
 b=[44,55,66]
 q9= a.concat(b)
 console.log(q9)
+// join()
+let info =["Asavari","Bedade", 8108580033]
+q10= info.join('-')
+console.log(q10)
+
+
+
+// DAY2
+// pgm1
+let birthyear= ['1989','1990','1991','1992']
+ages=[]
+
+for (i=0;i<birthyear.length;i++){
+    // console.log(i)
+    // console.log(birthyear[i])
+    // console.log(2024-birthyear[i])
+    x= 2024-birthyear[i]
+    ages.push(x)
+}
+console.log(ages)
+
+// pgm2
+let marks=[11,22,33,44,55,66,77,88,99]
+let above50=[]
+for(i=0;i < marks.length;i++){
+    if(marks[i]>50){
+    above50.push(marks[i])
+    }
+}
+console.log(above50)
+
+// pgm3
+let numbers=[11,22,33]
+total=0
+for(i=0;i<numbers.length;i++){
+    // console.log(i)
+    // console.log(numbers[i])
+    total=total+numbers[i]
+
+}
+console.log(total)
+
+// pgm4
+let cities=["mumbai","nagpur","pune","solapur"]
+for(i=0;i<cities.length;i++){
+    console.log("Welcome "+cities[i])
+}
+
